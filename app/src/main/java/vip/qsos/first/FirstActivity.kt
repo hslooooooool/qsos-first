54a5d97007c3d0893b3ff5fe81d9b31ea8e3a3ca
华清松 <821034742@qq.com>
2020-07-28 22:08:14 +0800

    修改冲突依赖

--- app/src/main/java/vip/qsos/first/FirstActivity.kt
+++ app/src/main/java/vip/qsos/first/FirstActivity.kt
@@ -3,18 +3,10 @@ package vip.qsos.first
 import android.os.Bundle
 import qsos.lib.base.base.activity.BaseActivity
 
-/**
- * @author : 华清松
- * TODO 类说明，描述此类的类型和用途
- */
-class FirstActivity : BaseActivity() {
-    override val layoutId: Int?
-        get() = R.layout.activity_first
-    override val reload: Boolean
-        get() = false
+class FirstActivity : BaseActivity(R.layout.activity_first) {
 
-    override fun getData() {
-
+    override fun getData(loadMore: Boolean) {
+        TODO("Not yet implemented")
     }
 
     override fun initData(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/vip/qsos/first/MyApplication.kt
@@ -0,0 +1,16 @@
+package vip.qsos.first
+
+import android.app.Application
+import android.util.Log
+//import vip.qsos.utils_exception.lib.GlobalExceptionHelper
+
+open class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        /**全局异常捕获处理*/
+      //  GlobalExceptionHelper.init(this, Log.ASSERT, Log.ERROR)
+    }
+
+}
